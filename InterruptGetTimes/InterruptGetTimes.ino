5add78dff6e0f7d14ffa49933c247a64ed839641
Helena Andersen <h.azephra@gmail.com>
2018-11-23 22:27:59 +0100

    Update InterruptGetTimes.ino
    added blinkCounter

--- InterruptGetTimes/InterruptGetTimes.ino
+++ InterruptGetTimes/InterruptGetTimes.ino
@@ -1,8 +1,10 @@
+#include <ESP8266WiFi.h>
 const byte ledPin = 0;
 const byte interruptPin = 4;
 volatile byte state = LOW;
 unsigned long lastBlinkTime = 0;
 unsigned static int compareTime = 100;
+char blinkCounter = 0;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
@@ -18,6 +20,7 @@ void loop() {
 void blink() {
   if (millis() > lastBlinkTime + compareTime) {
     lastBlinkTime = millis();
+    blinkCounter++;
     state = !state;
     Serial.println("Blink");
   }
